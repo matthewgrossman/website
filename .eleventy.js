983cd2fa67628e83ef953aeaead92b65bb908c78
Matthew Grossman <matthewryangrossman@gmail.com>
2020-10-11 21:46:59 -0700

    add in cname

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("static");
+    eleventyConfig.addPassthroughCopy("CNAME");
     // return {
     //     dir: {
     //         output: "docs"
